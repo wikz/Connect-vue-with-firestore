66ecadce484a6462e94f408f646b77b079a38f04
wikz87 <wikz@live.com>
2020-07-09 23:28:14 +0500

    Fix input field's line height

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -4,7 +4,7 @@
       {{ label }}
     </label>
     <input
-      class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:shadow-outline"
       :name="id"
       :type="type"
       :placeholder="placeholder"
